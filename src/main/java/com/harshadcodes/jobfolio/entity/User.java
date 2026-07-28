3d61f998ea917edabf28ed4823dc3152c5b37999
Harshad Yewale <harshadyewale73@gmail.com>
2026-07-28 23:09:17 +0530

    configured database and flyway migration and created user entity with repository

---
+++ src/main/java/com/harshadcodes/jobfolio/entity/User.java
@@ -0,0 +1,29 @@
+package com.harshadcodes.jobfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
