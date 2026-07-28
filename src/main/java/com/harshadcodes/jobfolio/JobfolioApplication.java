a571a2224b80c8312d30547a5349f39b525bfcdf
Harshad Yewale <harshadyewale73@gmail.com>
2026-07-28 22:51:20 +0530

    initial commit

---
+++ src/main/java/com/harshadcodes/jobfolio/JobfolioApplication.java
@@ -0,0 +1,13 @@
+package com.harshadcodes.jobfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshadcodes/jobfolio/JobfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshadcodes.jobfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
